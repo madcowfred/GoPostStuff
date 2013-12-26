bf6fad9c20d76cd41dd28fbc18bd4ad0dc32aeec
Freddie <freddie@wafflemonster.org>
2013-12-27 00:59:50 +1030

    Add config stuff

--- gopoststuff.go
+++ gopoststuff.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    //"github.com/madcowfred/yencode"
-    "github.com/op/go-logging"
+	"flag"
+	"path/filepath"
+	"os/user"
+	//"fmt"
+	//"github.com/madcowfred/yencode"
+	"code.google.com/p/gcfg"
+	"github.com/op/go-logging"
 )
 
 // Command ling flags
@@ -16,25 +19,75 @@ var dirSubjectFlag = flag.Bool("d", false, "Use directory names as subject prefi
 // Logger
 var log = logging.MustGetLogger("gopoststuff")
 
+// Config
+var Config struct {
+	Global struct {
+		From string
+		DefaultGroup string
+		ArticleSize int32
+	}
+
+	Server map[string]*struct {
+		Address string
+		Port uint16
+		Username string
+		Password string
+		Connections uint8
+		TLS bool
+	}
+}
+
 func main() {
-    // Parse command line flags
-    flag.Parse()
+	// Parse command line flags
+	flag.Parse()
 
-    // fmt.Printf("Verbose: %v\n", *verboseFlag)
-    // fmt.Printf("Config: %v\n", *configFlag)
-    // fmt.Printf("Subject: %s\n", *subjectFlag)
-    // fmt.Printf("DirSubject: %v\n", *dirSubjectFlag)
-    // fmt.Printf("Remaining args: %v\n", args)
+	// Set up logging
+	var format = logging.MustStringFormatter(" %{level: -8s}  %{message}")
+	logging.SetFormatter(format)
+	if *verboseFlag {
+		logging.SetLevel(logging.DEBUG, "gopoststuff")
+	} else {
+		logging.SetLevel(logging.INFO, "gopoststuff")
+	}
 
-    // Set up logging
-    var format = logging.MustStringFormatter("[%{level: -8s}] %{message}")
-    logging.SetFormatter(format)
-    if *verboseFlag {
-        logging.SetLevel(logging.DEBUG, "gopoststuff")
-    } else {
-        logging.SetLevel(logging.INFO, "gopoststuff")
-    }
+	log.Info("gopoststuff starting...")
 
-    log.Debug("test debug")
-    log.Info("test info")
+	// fmt.Printf("Verbose: %v\n", *verboseFlag)
+	// fmt.Printf("Config: %v\n", *configFlag)
+	// fmt.Printf("Subject: %s\n", *subjectFlag)
+	// fmt.Printf("DirSubject: %v\n", *dirSubjectFlag)
+	// fmt.Printf("Remaining args: %v\n", flag.Args())
+
+	// Make sure -d or -s was specified
+	if len(*subjectFlag) == 0 && !*dirSubjectFlag {
+		log.Fatal("Need to specify -d or -s option, try --help!")
+	}
+
+	// Load config file
+	var cfgFile string
+	if len(*configFlag) > 0 {
+		cfgFile = *configFlag
+	} else {
+		// Default to user homedir for config file
+		u, err := user.Current()
+		if err != nil {
+			log.Fatal(err)
+		}
+		cfgFile = filepath.Join(u.HomeDir, ".gopoststuff.conf")
+	}
+
+	err := gcfg.ReadFileInto(&Config, cfgFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// log.Debug("Config.Global: %+v", Config.Global)
+	// for n, s := range Config.Server {
+	//	 log.Debug("Config.Server %s: %+v", n, s)
+	// }
+
+	// Initialise connections
+	// for name, server := range Config.Server {
+
+	// }
 }
